4f1228166e7d4c08241af09a09de2d9e39c44881
PresentJay <PresentJay@Etri.re.kr>
2020-08-25 17:52:17 +0900

    feat: settings for fhir-resource-tester (configure express framework)_

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const client = require('fhir-kit-client');
+const Client = require('fhir-kit-client');
+const fhirClient = new Client({
+  baseUrl: 'http://129.254.63.252:8888'
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'etri-fhir-resource-tester'
+  });
 });
 
 module.exports = router;
